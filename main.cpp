3bd2b5b8376e7603fcfe8c2c9ec94fae0c1c8312
sergeyzoloto <sergiozoloto@gmail.com>
2023-07-23 18:53:39 +0200

    SDL video system init

---
+++ main.cpp
@@ -0,0 +1,19 @@
+// C++ Standard Libraries
+#include <iostream>
+
+// Third-party library
+#include <SDL2/SDL.h>
+
+int main(int argc, char *argv[])
+{
+  if (SDL_Init(SDL_INIT_VIDEO) < 0)
+  {
+    std::cout << "SDL could not be initialized: " << SDL_GetError();
+  }
+  else
+  {
+    std::cout << "SDL video system is ready to go\n";
+  }
+
+  return 0;
+}
